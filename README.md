20f3d2ac29e38aad86d4d1a79dd2a6d447ebbccb
Ivan Zelona <ivan_zelona@hotmail.com>
2024-08-09 16:41:40 +0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-at first git hub token, and aws access keys were added as environment variables to the system to avoid sensitive data as plain text
-once access keys and token were added as secrets to repository, I used workflow to add them to the action runner as variables for further use
+at first git hub token, and aws access keys were added as environment variables to the system to avoid sensitive data as plain text in the project.
+once access keys and token were added as secrets to relevant repository, I used workflow to add them to the action runner as variables for further use
--- main.tf
+++ main.tf
@@ -14,9 +14,15 @@ resource "github_actions_secret" "aws_secret_access_key" {
   plaintext_value = var.aws_secret_access_key
 }
 
+variable "repositories" {
+  type    = list(string)
+  default = ["PR-Terraform", "PR-Service"]
+}
+
 resource "github_actions_secret" "pr_access_token" {
-  repository     = "PR-Terraform"
-  secret_name    = "PR_ACCESS_TOKEN"
+  for_each        = toset(var.repositories)
+  repository      = each.value
+  secret_name     = "PR_ACCESS_TOKEN"
   plaintext_value = var.pr_access_token
 }
 
@@ -31,6 +37,79 @@ variable "aws_access_key_id" {
 }
 
 variable "aws_secret_access_key" {
-  type = string
+  type        = string
   description = "AWS Secret Access Key."
+}
+#########################################
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_caller_identity" "current" {}
+
+
+# OIDC Identity Provider for GitHub Actions
+
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com"
+  ]
+
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1"
+  ]
+}
+# S3 bucket for logs
+resource "aws_s3_bucket" "logs_bucket" {
+  bucket = "pr-service-bucket-alex"
+}
+
+# OIDC IAM Role for GitHub Actions
+resource "aws_iam_role" "github-oidc-role" {
+  name = "github-oidc-role"
+  assume_role_policy = <<EOF
+
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Federated": "arn:aws:iam::654654242440:oidc-provider/token.actions.githubusercontent.com"
+      },
+      "Action": "sts:AssumeRoleWithWebIdentity",
+      "Condition": {
+        "StringLike": {
+          "token.actions.githubusercontent.com:sub": "repo:thanevil/PR-Service:*"
+        }
+      }
+    }
+  ]
+}
+
+EOF
+}
+
+# Attach a policy to the role to allow it to upload files to S3
+resource "aws_iam_role_policy" "github-oidc-role_policy" {
+  name   = "github-oidc-role_policy"
+  role   = aws_iam_role.github-oidc-role.id
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:PutObject",
+        "s3:PutObjectAcl"
+      ],
+      "Resource": "arn:aws:s3:::pr-service-bucket-alex/*"
+    }
+  ]
+}
+EOF
 }
